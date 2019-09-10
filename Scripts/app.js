09abc2e79efdd5f2ba9a543347a647265103578b
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-09-10 01:18:07 -0400

    updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,15 +2,32 @@
 // This is a closure; IIFE = Immediately Invoked Function Expression
 //Anonymous self executing function
 (function(){
-    
+    "use strict"
     function Start(){
         //local variable
-        var title = document.title;
+
+       // var title = document.title;
+       let title = document.title; // we need to use "use strict" above
+
+       switch(title){
+           case "COMP125 - Home":
+                let AboutButton = document.getElementById("AboutButton");
+
+                AboutButton.addEventListener("click", function(){
+                     console.log("About Button was clicked!");
+         
+                })
+               break;
+            case "COMP125 - About":
+                break;
+       }
+       
 
         console.log("App Started!");
         console.log("--------------------------------");
         console.log("Title: "+ title);
     }
-    Start();
+    //Start(); // or below
+    window.onload = Start;
 
 })();
