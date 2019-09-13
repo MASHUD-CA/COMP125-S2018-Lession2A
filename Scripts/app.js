52ad9bd4aa3a1c2adb1c0473a1389b5d77beda23
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-09-12 22:50:36 -0400

    updated project

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,7 @@
 (function () {
     "use strict"
     //function scope
-    let AboutButton;
+    //let AboutButton;
     //Two ways to do functions (Named way + )
     // function HomeContent(){ } // HomeContent function. HomeContent -> Identifier
     // let HomeContent = function(){ }  // Anonymas function refer to name HomeContent
@@ -14,18 +14,19 @@
         console.log("About Button clicked");
     }
     function AboutButtonOver(event) {
-        //  event.currentTarget.style.opacity = 0.3;
-        AboutButton.style.opacity = 0.3 // If we declare AboutButton top.
+        //AboutButton.style.opacity = 0.3 // If we declare AboutButton top.
+        event.currentTarget.style.opacity = 0.3;
     }
 
     function AbooutButtonOut(event) {
         event.currentTarget.style.opacity = 1.0;
     }
     function HomeContent() {
-        AboutButton = document.getElementById("AboutButton");
+        let AboutButton = document.getElementById("AboutButton");
 
-        AboutButton.innerText = "About";
-
+        //Some browser do not like innerText but others do. textContent is standard. User textContent
+        //AboutButton.innerText = "About";
+        AboutButton.textContent = "About"
 
         //About Button event listener (Call back function)
         AboutButton.addEventListener("click", AboutButtonClick);
@@ -33,7 +34,22 @@
         AboutButton.addEventListener("mouseout", AbooutButtonOut);
 
     }
+    function ReturnPI() {
+        return Math.PI;
+    }
+    function Aboutcontent() {
+        let myNumber = ReturnPI();
+        console.log("My Funky Number: " + myNumber);
 
+        let paragraph = document.getElementById("paragraph");
+
+        //Some browser do not like innerText but others do. textContent is standard. User textContent
+        //paragraph.innerText = "We will be changing the content of this paragraph one day";
+        //paragraph.textContent = "We will be changing the content of this paragraph one day";
+
+        let mySentence = "This is About Page / About Page / About Page ";
+        paragraph.textContent = mySentence;
+    }
 
     function Start() {
         //local variable
@@ -43,22 +59,18 @@
         console.log("--------------------------------");
         console.log("Title: " + title);
 
-
-
         switch (title) {
             case "COMP125 - Home":
                 HomeContent();
                 break;
-           
+
             case "COMP125 - About":
+                Aboutcontent();
                 break;
 
             default:
                 break;
-
         }
-
-
     }
     /* Learning different way of calling functions
     function outer(){
